e963cdbdb55d4b83b01841769a341d18b4d99621
Yash Wason <yashwason@gmail.com>
2025-08-24 19:14:19 +0530

    feat: continously ask for commands

--- main.go
+++ main.go
@@ -7,25 +7,32 @@ import (
 )
 
 func main() {
-	var command string
-
-	fmt.Println("Supported commands: drive/reverse/left/right")
+	fmt.Println("Supported commands: drive/reverse/left/right/exit")
 	fmt.Println("--------------------------------------------")
 
-	fmt.Println("Enter command:")
+commandLoop:
+	for {
+		var command string
 
-	fmt.Scanln(&command)
+		fmt.Println("Enter command:")
 
-	switch strings.ToLower(command) {
-	case `left`:
-		motor.SteerLeft()
-	case `right`:
-		motor.SteerRight()
-	case `drive`:
-		motor.DriveAhead()
-	case `reverse`:
-		motor.DriveReverse()
-	default:
-		fmt.Printf(`Cannot do '%s'`, command)
+		fmt.Scanln(&command)
+
+		switch strings.ToLower(command) {
+		case `left`:
+			motor.SteerLeft()
+		case `right`:
+			motor.SteerRight()
+		case `drive`:
+			motor.DriveAhead()
+		case `reverse`:
+			motor.DriveReverse()
+		case `exit`:
+			break commandLoop
+		default:
+			fmt.Printf("Cannot do '%s'\n", command)
+		}
 	}
+
+	fmt.Println("Rovi exited. Thank you!")
 }
