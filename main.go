e0381a227b49ca73f114ef03be4e0ab9985d65ec
Yash Wason <yashwason@gmail.com>
2025-09-14 21:45:01 +0530

    feat: control motor using potentiometer & PWM

--- main.go
+++ main.go
@@ -6,34 +6,57 @@ import (
 	"time"
 )
 
+const RANGE_OF_REVERSE = 5500
+const RANGE_OF_STOP = 5750
+const NON_FORWARD_RANGE = RANGE_OF_REVERSE + RANGE_OF_STOP
+const UINT32_UPPER_LIMIT = 65535
+
 func main() {
 	time.Sleep(time.Millisecond * 20)
 
-	motor.InitDriveMotors();
+	machine.InitADC()
 
-	println("Use keyboard keys a, s, d, w & spacebar to control")
-	println("--------------------------------------------")
+	drivePote := machine.ADC{
+		Pin: machine.ADC0,
+	}
 
-	time.Sleep(time.Millisecond * 20);
+	pin6Top, setPin6 := motor.SetupPin6()
+	_, setPin7 := motor.SetupPin7()
+	_, setPin20 := motor.SetupPin20()
+	_, setPin21 := motor.SetupPin21()
 
 	for {
-		b, err := machine.Serial.ReadByte()
+		time.Sleep(time.Millisecond * 10)
+		drivePoteVal := uint32(drivePote.Get())
 
-		if err == nil {
-			switch rune(b) {
-			case 'a', 'A':
-				motor.SteerLeft()
-			case 'd', 'D':
-				motor.SteerRight()
-			case 'w', 'W':
-				motor.DriveAhead()
-			case 's', 'S':
-				motor.DriveReverse()
-			case ' ':
-				motor.StopDriving()
-			default:
-				println("Unknown command")
-			}
+		if drivePoteVal <= RANGE_OF_REVERSE {
+			reverseSpeed := mapValue(drivePoteVal, 0, RANGE_OF_REVERSE, RANGE_OF_REVERSE*10, 0)
+
+			setPin6(0)
+			setPin20(0)
+
+			setPin7(reverseSpeed)
+			setPin21(reverseSpeed)
+		} else if drivePoteVal <= RANGE_OF_STOP {
+			setPin6(0)
+			setPin20(0)
+			setPin7(0)
+			setPin21(0)
+		} else {
+			forwardSpeed := mapValue(drivePoteVal, RANGE_OF_STOP+1, pin6Top, 0, pin6Top)
+
+			setPin7(0)
+			setPin21(0)
+
+			setPin6(forwardSpeed)
+			setPin20(forwardSpeed)
 		}
+
+		// 	motor.SteerLeft()
+		// 	motor.SteerRight()
 	}
+}
+
+func mapValue(x, inMin, inMax, outMin, outMax uint32) uint32 {
+	return uint32(int64(x-inMin)*int64(int32(outMax)-int32(outMin))/int64(inMax-inMin) + int64(outMin))
 }
--- motor/motor.go
+++ motor/motor.go
@@ -1,17 +1,57 @@
 package motor
 
-import "machine"
+import (
+	"machine"
+	"time"
+)
 
-const pin20 = machine.GPIO20
-const pin21 = machine.GPIO21
-const pin6 = machine.GPIO6
-const pin7 = machine.GPIO7
+type pwm interface {
+	Configure(config machine.PWMConfig) error
+	Channel(pin machine.Pin) (channel uint8, err error)
+	Set(channel uint8, value uint32)
+}
 
-func InitDriveMotors() (machine.Pin, machine.Pin, machine.Pin, machine.Pin) {
-	pin20.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	pin21.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	pin6.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	pin7.Configure(machine.PinConfig{Mode: machine.PinOutput})
+var pin20 = machine.GPIO20
+var pin21 = machine.GPIO21
+var pin6 = machine.GPIO6
+var pin7 = machine.GPIO7
 
-	return pin20, pin21, pin6, pin7
+var period = uint64(time.Second.Nanoseconds() / 500) // 500Hz
+
+// setupPWM is a generic helper function that configures a pin for PWM.
+// It uses the pwm interface to work with any PWM peripheral that provides the necessary methods.
+func setupPWM(pin machine.Pin, p pwm, pinName string) (setFunc func(value uint32)) {
+	pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	if err := p.Configure(machine.PWMConfig{Period: period}); err != nil {
+		println("Error configuring PWM on "+pinName+": ", err.Error())
+		return nil
+	}
+	channel, err := p.Channel(pin)
+
+	if err != nil {
+		println("Error getting PWM channel on "+pinName+": ", err.Error())
+		return nil
+	}
+
+	return func(value uint32) {
+		p.Set(channel, value)
+	}
+}
+
+type setValue func(value uint32)
+
+func SetupPin20() (top uint32, set setValue) {
+	return machine.PWM2.Top(), setupPWM(pin20, machine.PWM2, "Pin20")
+}
+
+func SetupPin21() (top uint32, set setValue) {
+	return machine.PWM2.Top(), setupPWM(pin21, machine.PWM2, "Pin21")
+}
+
+func SetupPin6() (top uint32, set setValue) {
+	return machine.PWM3.Top(), setupPWM(pin6, machine.PWM3, "Pin6")
+}
+
+func SetupPin7() (top uint32, set setValue) {
+	return machine.PWM3.Top(), setupPWM(pin7, machine.PWM3, "Pin7")
 }
